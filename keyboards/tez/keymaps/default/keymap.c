2853d0d184957ccd57eb2dac82c74b9219d1fecc
Yehonatan Elentok <elyoni@gmail.com>
2024-08-19 21:47:05 +0300

    [Tez] Alighed my keybinds

--- keyboards/tez/keymaps/default/keymap.c
+++ keyboards/tez/keymaps/default/keymap.c
@@ -24,7 +24,7 @@ enum layer_names {
     GAME_NUM,
 };
 
-
+#define KC_TRAN KC_TRANSPARENT
 #define KC_LSHIFT KC_LEFT_SHIFT
 #define KC_RSHIFT KC_RIGHT_SHIFT
 #define KC_LCTRL KC_LEFT_CTRL
@@ -60,8 +60,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_Q,                KC_W,                KC_E,                KC_R,                KC_T,                       KC_Y,                KC_U,                KC_I,                KC_O,                MT(MOD_LALT, KC_P),
             MT(MOD_LCTL, KC_A),  MT(MOD_LGUI, KC_S),  MT(MOD_LSFT, KC_D),  LT(NUMPAD,KC_F),     KC_G,                       KC_H,                KC_J,                MT(MOD_RSFT,KC_K),   MT(MOD_RGUI, KC_L),  MT(MOD_RCTL, KC_SCOLON),
             MT(MOD_LALT, KC_Z),  KC_X,                KC_C,                LT(SYMB,KC_V),       KC_B,                       KC_N,                KC_M,                KC_COMMA,            KC_DOT,              LT(MOUSE_F,KC_SLASH),
-                                                  MO(OTHER),           LT(NUMPAD,KC_SPACE), KC_LSHIFT,                  LT(SYMB,KC_BSPACE),  LT(MOVE,KC_ENTER),   KC_NO
-    ),
+                                                      MO(OTHER),           LT(NUMPAD,KC_SPACE), KC_LSHIFT,                  LT(SYMB,KC_BSPACE),  LT(MOVE,KC_ENTER),   KC_NO
+),
     [MOVE] = LAYOUT_v3(
             KC_TRANSPARENT,      KC_EXLM,             KC_LBRACKET,         KC_RBRACKET,         LSFT(KC_TAB),               KC_HOME,             KC_PGDOWN,           KC_PGUP,             KC_DELETE,           LSFT(KC_F10),
             KC_LCTRL,            LSFT(KC_LALT),       KC_LPRN,             KC_RPRN,             KC_TAB,                     KC_LEFT,             KC_DOWN,             KC_UP,               KC_RIGHT,            KC_RCTRL,
@@ -69,10 +69,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                       KC_PRINT_SCREEN,     KC_TRANSPARENT,      KC_TRANSPARENT,             KC_TRANSPARENT,      KC_TRANSPARENT,      KC_NO
     ),
     [NUMPAD] = LAYOUT_v3(
-            KC_BSPACE,           KC_ESCAPE,           KC_ENTER,            KC_TRANSPARENT,      KC_GRAVE,                   KC_SLASH,            KC_7,                MT(MOD_RSFT, KC_8),  KC_9,                KC_ASTR,
-            KC_LCTRL,            KC_LGUI,             KC_LSHIFT,           KC_TRANSPARENT,      KC_F,                       KC_EQUAL,            KC_4,                KC_5,                KC_6,                MT(MOD_RCTL, KC_MINUS),
-            KC_LALT,             KC_CUT,              KC_COPY,             KC_PASTE,            KC_TRANSPARENT,             KC_PLUS,             KC_1,                KC_2,                KC_3,                KC_KP_DOT,
-                                                      KC_NUMLOCK,          KC_TRANSPARENT,      QK_BOOTLOADER,              KC_TRANSPARENT,      KC_TRANSPARENT,      KC_0
+            KC_BSPACE,           KC_ESCAPE,           KC_ENTER,            KC_COPY,      KC_PASTE,                   KC_PLUS,             KC_7,                MT(MOD_RSFT, KC_8),  KC_9,                KC_SLASH ,
+            KC_LCTRL,            KC_LGUI,             KC_LSHIFT,           KC_GRAVE,     KC_CUT,                     KC_MINUS,            KC_3,                KC_5,                KC_6,                MT(MOD_RCTL, KC_EQUAL),
+            KC_LALT,             KC_NUMLOCK,          KC_TRAN,             KC_TRAN,      KC_GRAVE,                   KC_COMMA,            KC_1,                KC_2,                KC_3,                KC_KP_DOT,
+                                                      KC_NUMLOCK,          KC_TRAN,      QK_BOOTLOADER,              KC_TRAN,      KC_TRAN,      KC_0
     ),
     [MOUSE_F] = LAYOUT_v3(
             KC_MS_WH_LEFT,       KC_MS_WH_DOWN,       KC_MS_UP,            KC_MS_WH_UP,         KC_MS_WH_RIGHT,              KC_F11,             KC_F7,               KC_F8,               KC_F9,               KC_F12,
@@ -81,9 +81,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                       KC_TRANSPARENT,      KC_MS_BTN1,          KC_MS_BTN2,                  KC_MS_BTN1,         KC_MS_BTN2,          KC_MS_BTN1
     ),
     [SYMB] = LAYOUT_v3(
-            KC_CH_LNG,           KC_ESCAPE,           KC_ENTER,            KC_TRANSPARENT,      KC_TRANSPARENT,             KC_BSLASH,           RSFT(KC_BSLASH),     KC_QUOTE,            KC_DQUO,             KC_TRANSPARENT,
-            KC_M_ALL,            KC_TRANSPARENT,      KC_PASTE,            KC_TRANSPARENT,      KC_TRANSPARENT,             KC_CIRC,             KC_ASTR,             KC_HASH,             KC_DLR,              KC_UNDS,
-            KC_PC_UNDO,          KC_CUT,              KC_COPY,             KC_PASTE,            KC_TRANSPARENT,             KC_TRANSPARENT,      KC_TILD,             KC_TRANSPARENT,      KC_TRANSPARENT,      KC_TRANSPARENT,
+            KC_BSLASH,           KC_ESCAPE,           KC_ENTER,            KC_TRANSPARENT,      KC_TRANSPARENT,              KC_TRANSPARENT,      RSFT(KC_BSLASH),     KC_QUOTE,            KC_DQUO,             KC_BSLASH,
+            KC_M_ALL,            KC_TRANSPARENT,      KC_TRAN,             KC_TRANSPARENT,      KC_TRANSPARENT,             KC_UNDS,             KC_ASTR,             KC_HASH,             KC_DLR,              KC_UNDS,
+            KC_PC_UNDO,          KC_CUT,              KC_COPY,             KC_PASTE,            KC_TRANSPARENT,             KC_TRANSPARENT,      KC_TILD,             RSFT(KC_BSLASH),           KC_F4,         KC_EQUAL,
                                                       KC_TRANSPARENT,      KC_TRANSPARENT,      KC_TRANSPARENT,             KC_TRANSPARENT,      KC_TRANSPARENT,      KC_TRANSPARENT
     ),
     [OTHER] = LAYOUT_v3(
@@ -101,10 +101,50 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-// clang-format on
-    /*"split": {*/
-        /*"soft_serial_pin": "B5",*/
-        /*"usb_detect": {*/
-            /*"enabled": true*/
-        /*}*/
-    /*},*/
+
+#ifdef RGBLIGHT_ENABLE
+#define LED_BRIGHTNESS 10
+#define HSV_LED_ORANGE 28, 255, 16
+#define HSV_LED_OFF 0, 0, 0
+#define HSV_LED_RED 0, 255, LED_BRIGHTNESS
+#define HSV_LED_GREEN 85, 255, LED_BRIGHTNESS
+#define HSV_LED_CYAN 128, 255, LED_BRIGHTNESS
+#define HSV_LED_YELLOW 63, 100, LED_BRIGHTNESS
+#define HSV_LED_TEST 100, 50, LED_BRIGHTNESS
+#endif
+
+#ifdef RGBLIGHT_ENABLE
+layer_state_t layer_state_set_user(layer_state_t state) {
+  uint8_t layer = get_highest_layer(state);
+  switch (layer) {
+      case BASE:
+          rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+          rgblight_sethsv(HSV_LED_OFF);
+        break;
+      case MOVE:
+          rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING+3);
+          rgblight_sethsv(HSV_LED_RED);
+        break;
+      case NUMPAD:
+          rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING+3);
+          rgblight_sethsv(HSV_LED_GREEN);
+        break;
+      case MOUSE_F:
+          rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING+3);
+          rgblight_sethsv(HSV_LED_CYAN);
+        break;
+      case SYMB:
+          rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING+3);
+          rgblight_sethsv(HSV_LED_YELLOW);
+        break;
+  case GAMEM_L:
+  case GAMEM_R:
+          rgblight_mode_noeeprom(RGBLIGHT_MODE_KNIGHT);
+          rgblight_sethsv(HSV_LED_TEST);
+        break;
+      default:
+        break;
+    }
+  return state;
+};
+#endif
