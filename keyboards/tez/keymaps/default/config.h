f4be8f2ad0097df0db338ef9c26a3b6739af7117
Yehonatan Elentok <elyoni@gmail.com>
2024-08-19 21:47:05 +0300

    [Tez] Alighed my keybinds

--- keyboards/tez/keymaps/default/config.h
+++ keyboards/tez/keymaps/default/config.h
@@ -1,5 +1,11 @@
 #pragma once
 
 #define EE_HANDS
-#define TAPPING_TERM 200
+#define TAPPING_TERM 150
+#define DEBOUNCE 5
 #define PERMISSIVE_HOLD
+
+// wiring of each half
+#define MATRIX_ROW_PINS { B3, B2, B6, B4 }
+#define MATRIX_COL_PINS { D0, D4, C6, D7, E6 }
+#define MATRIX_COL_PINS_RIGHT { E6, D7, C6, D4, D0 }
