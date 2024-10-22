c070f888c41b190c3ab14b2f5f8d433ed93d3b34
Yehonatan Elentok <elyoni@gmail.com>
2024-10-22 21:23:17 +0300

    [Tez] Move configuration from config.h to keyboard.json

--- keyboards/tez/keymaps/default/config.h
+++ keyboards/tez/keymaps/default/config.h
@@ -1,14 +1,5 @@
 #pragma once
 
 #define EE_HANDS
-//#define TAPPING_TERM 150
-#define DEBOUNCE 5
-//#define PERMISSIVE_HOLD
-//#define RGBLIGHT_VAL_STEP 5
-
-
-//#define COMBO_TERM 120
-// wiring of each half
-#define MATRIX_ROW_PINS { B3, B2, B6, B4 }
-#define MATRIX_COL_PINS { D0, D4, C6, D7, E6 }
-#define MATRIX_COL_PINS_RIGHT { E6, D7, C6, D4, D0 }
+//#define DEBOUNCE 5
+#define COMBO_SHOULD_TRIGGER
